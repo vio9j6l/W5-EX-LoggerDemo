46ee69ab89fe180d8ce79b9888724fe801f52c64
vio9j6l <april951ss@gmail.om>
2020-02-11 08:32:22 -0500

    Initial commit

---
+++ src/com/company/Logger.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Logger {
+    private Logger() {
+        System.out.println("New Instance Created");
+    }
+}
---
+++ src/com/company/LoggerDemo.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class LoggerDemo {
+    public static void main(String[] args) {
+        Logger myLogger = new Logger();
+        Logger myLogger2 = new Logger();
+        Logger myLogger3 = new Logger();
+    }
+}
